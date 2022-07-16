569c3069246e3aef9a8ab46cfa3d96fde01d82f8
Miguel Sosa <miguelsosa2003@gmail.com>
2022-07-16 14:22:31 -0500

    Cree el js y css del layout

--- src/components/layout/BarraDeNavegacion.js
+++ src/components/layout/BarraDeNavegacion.js
@@ -1,11 +1,19 @@
+import { Link } from "react-router-dom";
+
 function BarraDeNavegacion() {
   return (
     <nav>
       <h1>Acuarelas</h1>
       <ul>
-        <li>Inicio</li>
-        <li>Sobre Mi</li>
-        <li>Contacto</li>
+        <li>
+          <Link to="/">Inicio</Link>
+        </li>
+        <li>
+          <Link to="/sobre-mi">Sobre Mi</Link>
+        </li>
+        <li>
+          <Link to="/contacto">Contacto</Link>
+        </li>
       </ul>
     </nav>
   );
